e8c4b38b2beece1be2da44ded03cacc66b90c09e
Elías Mouesca <emouesca@frba.utn.edu.ar>
2024-06-08 20:51:49 -0300

    Grupo, tenemos logger!!
    + Codigo de logger para la entrega que sigue, que hay que loggear
      bastante, para el uso, chequear el test.

---
+++ src/main/java/ar/edu/utn/frba/dds/simeal/service/LoggerType.java
@@ -0,0 +1,8 @@
+package ar.edu.utn.frba.dds.simeal.service;
+
+public enum LoggerType {
+  DEBUG,
+  INFORMATION,
+  ERROR,
+  WARNING
+}
---
+++ src/test/java/ar/edu/utn/frba/dds/service/LoggerTest.java
@@ -0,0 +1,60 @@
+package ar.edu.utn.frba.dds.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ar.edu.utn.frba.dds.simeal.service.LoggerType;
+import org.junit.jupiter.api.Test;
+import ar.edu.utn.frba.dds.simeal.service.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LoggerTest {
+  Logger logger;
+
+  @Test
+  public void logToFile()
+  {
+    String filename = "TEST_LOGGER.txt";
+    logger = Logger.getInstance(filename);
+
+    logger.log(LoggerType.DEBUG, "This is DEBUG information");
+    logger.log(LoggerType.WARNING, "This is a WARNING!");
+    logger.log(LoggerType.ERROR, "This is an ERROR!!");
+    logger.log(LoggerType.INFORMATION, "This is plain INFORMATION");
+
+    File logFile = new File(filename);
+    try {
+      assertTrue(logFile.exists(), "Log file should be created");
+
+      List<String> lines = Files.readAllLines(Paths.get(filename));
+      int expectedLines = 4;
+      assertEquals(expectedLines, lines.size());
+
+    } catch (IOException e) {
+      e.getStackTrace();
+    } finally {
+      if (logFile.exists()) {
+        assertTrue(logFile.delete());
+      }
+    }
+  }
+
+  // Esto no tiene un test la verdad, pero está bueno para ver el formato.
+  @Test
+  public void logToStdout()
+  {
+    logger = Logger.getInstance();
+
+    logger.log(LoggerType.DEBUG, "This is DEBUG information");
+    logger.log(LoggerType.WARNING, "This is a WARNING!");
+    logger.log(LoggerType.ERROR, "This is an ERROR!!");
+    logger.log(LoggerType.INFORMATION, "This is plain INFORMATION");
+
+  }
+
+
+}
