76fdd4e28d90b8b08a15c44552bafb0010ed35c7
Eli <emouesca@frba.utn.edu.ar>
2024-11-23 19:59:08 -0300

    Mejora middleware

--- src/main/java/ar/edu/utn/frba/dds/simeal/middleware/AuthenticatedMiddleware.java
+++ src/main/java/ar/edu/utn/frba/dds/simeal/middleware/AuthenticatedMiddleware.java
@@ -4,32 +4,33 @@ import ar.edu.utn.frba.dds.simeal.server.exception_handlers.NotAuthenticatedExce
 import ar.edu.utn.frba.dds.simeal.utils.logger.Logger;
 import io.javalin.Javalin;
 
+import java.util.regex.Pattern;
+
 // Middlewares are like questions, this' question is: are you authenticated?
 public class AuthenticatedMiddleware {
-
-    // Si lo mandamos a logearse y para logearse tiene que estar logeado... => O_o
-    private static final String[] openEndpoints = new String[]{
-            "/login", "/", "/index",
-            "/registro",
-            "/registro/humano",
-            "/registro/juridico",
-            "/user/create/humano",
-            "/user/create/juridico",
-            "/heladeras"
+    private static final Pattern[] openEndpoints = new Pattern[]{
+            Pattern.compile("/"),
+            Pattern.compile("/(index|login)"),
+            Pattern.compile("/registro(/(humano|juridico))?"),
+            Pattern.compile("/user/create.*"),
+            Pattern.compile("/heladeras"),
+            Pattern.compile(".*\\.(css|jpg|png|js|map|gif|ico)"),
     };
-    private static final String[] openFormats = new String[]{".css", ".jpg", ".png", ".js", ".map", ".gif", ".ico"};
+
+    public static boolean isOpenEndpoint(String input) {
+        for (Pattern pattern : openEndpoints) {
+            if (pattern.matcher(input).matches()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static void apply(Javalin app) {
         app.beforeMatched(
         ctx -> {
-            for (var format : openFormats) {
-                if (ctx.path().endsWith(format)) {
-                    return;
-                }
-            }
             Logger.trace("'"+ctx.ip()+"' trató de acceder a " + ctx.path());
-            if(
-              // TODO: Suscripcion/numero
+            /*if(ctx.path().startsWith("/heladera") ||
               ctx.path().startsWith("/suscripcion/") ||
               ctx.path().startsWith("/suscripciones") ||
               ctx.path().startsWith("/heladera/suscribirse/") ||
@@ -40,18 +41,17 @@ public class AuthenticatedMiddleware {
             {
                 return;
             }
-            for (var endpoint : openEndpoints) {
-                if (ctx.path().equals(endpoint)) {
-                    Logger.trace("'"+ctx.path()+"' es un endpoint abierto, no necesita estar autenticado.");
-                    return;
-                }
+             */
+            if (isOpenEndpoint(ctx.path())){
+                Logger.trace("'"+ctx.path()+"' es un endpoint abierto, no necesita estar autenticado.");
+                return;
             }
 
             Long userID = ctx.sessionAttribute("user_id");
-            Long colaboradorId = ctx.sessionAttribute("colaborador_id");
-            Long tecnicoId= ctx.sessionAttribute("tecnico_id");
             String username = ctx.sessionAttribute("user_name");
             String userType = ctx.sessionAttribute("user_type");
+            Long colaboradorId = ctx.sessionAttribute("colaborador_id");
+            Long tecnicoId= ctx.sessionAttribute("tecnico_id");
 
             if (userType == null){
                 ctx.sessionAttribute("redirect", ctx.path());
@@ -77,4 +77,5 @@ public class AuthenticatedMiddleware {
 
             });
     }
+
 }
